f0086ee94e1fd16f48a0e6da80516e4b3010df1b
Gauthier Bertet <gbertet@2E6.42angouleme.fr>
2023-03-29 16:40:20 +0200

    Fixed already sorted stacks issue

--- main.c
+++ main.c
@@ -12,6 +12,25 @@
 
 #include "push_swap.h"
 
+int     check_sorted(t_stack *stack_a, t_stack *stack_b)
+{
+	int		i;
+	t_stack *buff;
+
+	if (stack_b != NULL)
+		return (0);
+	i = 0;
+	buff = stack_a; 
+	while (buff != NULL)
+	{
+		if (buff->num != i)
+			return (0);
+		i++;
+		buff = buff->next;
+	}
+	return (1);
+}
+
 int	check_empty_args(int ac, char **av)
 {
 	int	i;
@@ -47,7 +66,8 @@ int	main(int ac, char **av)
 		error_exit();
 	stack_a = new_stack_a(ac - 1, av);
 	stack_b = NULL;
-	choose_sort(&stack_a, &stack_b);
+	if (!check_sorted(stack_a, stack_b))
+		choose_sort(&stack_a, &stack_b);
 	write_instruction(" ");
 	free_stack(stack_a);
 	free_stack(stack_b);
